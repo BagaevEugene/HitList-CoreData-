ca3b035dec59b46403dcb7fbc92f0921252439f8
BagaevEugene <36105583+BagaevEugene@users.noreply.github.com>
2019-12-20 23:09:26 +0700

    Initial Commit

---
+++ HitList(CoreData)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HitList(CoreData)
+//
+//  Created by Eugene Bagaev on 20.12.2019.
+//  Copyright © 2019 Eugene Bagaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
